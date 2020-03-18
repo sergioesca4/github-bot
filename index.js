1be1bc4ecae22f732424b873f14630acf358af93
Sergio Escalante <sergioescalante@ibm.com>
2020-03-18 09:53:32 -0400

    Update index.js to test if module works

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /**
  * @param {import('probot').Application} app
  */
+
 module.exports = app => {
   app.log('Yay, the app was loaded!')
-
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
+  app.log('The app worked!')
 }
